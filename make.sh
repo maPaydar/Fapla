8e78692e7ca346046d106b64c0b3778b51495ded
Amin Paydar <mapsgp920@gmail.com>
2016-11-27 15:05:26 +0330

    make system added

---
+++ make.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+Dir=$(pwd)
+Lang=$1
+OutPutLang=""
+AntlrGrammerPath="$Dir/fapla-antlr/Fapla.g4"
+AntlrJarPath="/fapla-antlr/lib/antlr-4.5.3-complete.jar"
+
+if [ "$Lang" = "Java" ]; then
+    OutPutLang="/fapla-java/src"
+elif [ "$Lang" = "Python" ]; then
+    Lang="Python2"
+    OutPutLang="/fapla-python"
+    echo expression evaluated as python
+elif [ "$Lang" = "JavaScript" ]; then
+    OutPutLang="/fapla-js"
+    echo expression evaluated as js
+elif [ "$Lang" = "CSharp" ]; then
+    OutPutLang="/fapla-cs"
+    echo expression evaluated as cs
+fi
+
+AntlrJar="java -jar $Dir$AntlrJarPath -Dlanguage=$Lang $AntlrGrammerPath -o $Dir$OutPutLang"
+echo $AntlrJar
+$AntlrJar
